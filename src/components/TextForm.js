f6fd387b3865fde631110f9089e09c002dd5ce84
archi2201 <archi.p@gatetouch.com>
2024-07-17 23:26:08 +0530

    video-8

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -22,6 +22,7 @@ export default function TextForm(props) {
   const [text, setText] = useState('Enter Text Here....');
 
   return (
+    <>
     <div className="container">
       <div className="form-floating m-5">
         <h2 className='pb-3'>{props.heading}</h2>
@@ -30,9 +31,18 @@ export default function TextForm(props) {
         <button className="btn btn-primary mt-3 ms-3" onClick={handleLwClick}>Convert To Lowercase</button>
         <button className="btn btn-primary mt-3 ms-3" onClick={handleClearClick}>Clear Text</button>
         <button className="btn btn-primary mt-3 ms-3" onClick={handleReverseClick}>Convert To Reverse</button>
-
-
       </div>
     </div>
+    <div className="container">
+      <div className="row my-3 m-5">
+        <h3>Your text Summury</h3>
+        <p>{text.split(/[.!?]+/).length || 0} sentences , {text.split(' ').length} words and {text.length} characters</p>
+        <p>{text.match(/[aeiouAEIOU]/g)?.length || 0} vowels and {text.match(/[bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ]/g)?.length || 0} constants</p>
+        <p>{text.split(' ').length * 0.008} Minutes to read</p>
+        <h3>Preview</h3>
+        <p>{text}</p>
+      </div>
+    </div>
+    </>
   );
 }
