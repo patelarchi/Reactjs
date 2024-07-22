52b6110f75e771b6b5a8f23e068628c79f11e64b
archi2201 <archi.p@gatetouch.com>
2024-07-22 22:42:28 +0530

    video-10

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import './App.css';
-import Navbar from './components/Navbar';
-import TextForm from './components/TextForm';
+  import './App.css';
+  import About from './components/About';
+  import Navbar from './components/Navbar';
+  import TextForm from './components/TextForm';
 
 
-function App() {
-  return (
-    <>
-      <Navbar title="TextUtils" aboutText="About TextUtils"/>
-      <TextForm heading="Enter Your Text to anaylze below"/>
-    </>
-  );
-}
+  function App() {
+    return (
+      <>
+        <Navbar title="TextUtils" aboutText="About TextUtils"/>
+        <TextForm heading="Enter Your Text to anaylze below"/>
+        <About/>
+      </>
+    );
+  }
 
-export default App;
+  export default App;
---
+++ src/components/About.js
@@ -0,0 +1,84 @@
+import React ,{useState} from 'react'
+
+export default function About() {
+
+    let initialStyle = {
+        color:'gray',
+        backgroundColor : 'white'
+    }
+
+    const [myStyle,setMyStyle] = useState(initialStyle);
+
+    const lightModeEnable = () => {
+        setMyStyle({
+            color:'gray',
+            backgroundColor: 'white',
+        });
+    };
+
+    const darkModeEnable = () => {
+        setMyStyle({
+            color:'white',
+            backgroundColor: 'black',
+        });
+    };
+
+    const toggleStyle = () => {
+      if(myStyle.color === 'white'){
+        lightModeEnable();
+
+      } else{
+        darkModeEnable();
+
+      }
+      console.log(myStyle.color);
+        
+    };
+  return (
+    <>
+    <div className="container" style={myStyle}>  
+        <div className="form-check form-switch ms-3">
+  <input className="form-check-input" type="checkbox" role="switch" onClick={toggleStyle} id="flexSwitchCheckDefault"/>
+</div><h2 className="text-center">About Page</h2>
+      <div className="accordion" id="accordionPanelsStayOpenExample" style={myStyle}>
+  <div className="accordion-item">
+    <h2 className="accordion-header">
+      <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
+        Accordion Item #1
+      </button>
+    </h2>
+    <div id="panelsStayOpen-collapseOne" className="accordion-collapse collapse show">
+      <div className="accordion-body">
+        <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+      </div>
+    </div>
+  </div>
+  <div className="accordion-item" style={myStyle}>
+    <h2 className="accordion-header">
+      <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
+        Accordion Item #2
+      </button>
+    </h2>
+    <div id="panelsStayOpen-collapseTwo" className="accordion-collapse collapse">
+      <div className="accordion-body">
+        <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+      </div>
+    </div>
+  </div>
+  <div className="accordion-item" style={myStyle}>
+    <h2 className="accordion-header">
+      <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
+        Accordion Item #3
+      </button>
+    </h2>
+    <div id="panelsStayOpen-collapseThree" className="accordion-collapse collapse">
+      <div className="accordion-body">
+        <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+    </>
+  )
+}
